2c2b006e1644cdb0922eb7c292960fcec0ce6d99
skibiks-games <skibiks.games@gmail.com>
2024-04-21 23:30:59 +0200

    Menu, sort

--- main.py
+++ main.py
@@ -134,9 +134,134 @@ class Graph:
                 dfs_recursion(start, visited)
         print()
 
+    def topological_sort_dfs_successors(self):
+        visited = [False] * self.vertices
+        stack = []
+
+        def topological_sort_util(v):
+            visited[v] = True
+            for i in self.successors(v):
+                if not visited[i]:
+                    topological_sort_util(i)
+            stack.append(v)
+
+        for i in range(self.vertices):
+            if not visited[i]:
+                topological_sort_util(i)
+
+        print("Sortowanie Topologiczne (DFS lista nastepnikow):")
+        while stack:
+            print(stack.pop(), end=' ')
+
+    def topological_sort_bfs_successors(self):
+        in_degree = [0] * self.vertices
+        for i in range(self.vertices):
+            for j in self.successors(i):
+                in_degree[j] += 1
+
+        queue = deque([i for i in range(self.vertices) if in_degree[i] == 0])
+        result = []
+
+        while queue:
+            v = queue.popleft()
+            result.append(v)
+            for i in self.successors(v):
+                in_degree[i] -= 1
+                if in_degree[i] == 0:
+                    queue.append(i)
+
+        print("Sortowanie Topologiczne (BFS lista nastepnikow):")
+        print(' '.join(map(str, result)))
+
+    def topological_sort_dfs_matrix(self):
+        visited = [False] * self.vertices
+        stack = []
+
+        def topological_sort_util(v):
+            visited[v] = True
+            for i in range(self.vertices):
+                if self.matrix[v][i] == 1 and not visited[i]:
+                    topological_sort_util(i)
+            stack.append(v)
+
+        for i in range(self.vertices):
+            if not visited[i]:
+                topological_sort_util(i)
+
+        print("Sortowanie Topologiczne (DFS macierz sasiedztwa):")
+        while stack:
+            print(stack.pop(), end=' ')
+        print()
+
+    def topological_sort_bfs_matrix(self):
+        in_degree = [0] * self.vertices
+        for i in range(self.vertices):
+            for j in range(self.vertices):
+                if self.matrix[i][j] == 1:
+                    in_degree[j] += 1
+
+        queue = deque([i for i in range(self.vertices) if in_degree[i] == 0])
+        order = []
+
+        while queue:
+            v = queue.popleft()
+            order.append(v)
+            for i in range(self.vertices):
+                if self.matrix[v][i] == 1:
+                    in_degree[i] -= 1
+                    if in_degree[i] == 0:
+                        queue.append(i)
+
+        print("Sortowanie Topologiczne (BFS macierz sasiedztwa):")
+        print(' '.join(map(str, order)))
+
+    def topological_sort_dfs_edges(self):
+        adj_list = {i: [] for i in range(self.vertices)}
+        for u, v in self.edges:
+            adj_list[u].append(v)
+
+        visited = [False] * self.vertices
+        stack = []
+
+        def topological_sort_util(v):
+            visited[v] = True
+            for i in adj_list[v]:
+                if not visited[i]:
+                    topological_sort_util(i)
+            stack.append(v)
+
+        for i in range(self.vertices):
+            if not visited[i]:
+                topological_sort_util(i)
+
+        print("Sortowanie Topologiczne (DFS tabela krawedzi):")
+        while stack:
+            print(stack.pop(), end=' ')
+        print()
+
+    def topological_sort_bfs_edges(self):
+        adj_list = {i: [] for i in range(self.vertices)}
+        in_degree = [0] * self.vertices
+        for u, v in self.edges:
+            adj_list[u].append(v)
+            in_degree[v] += 1
+
+        queue = deque([i for i in range(self.vertices) if in_degree[i] == 0])
+        order = []
+
+        while queue:
+            v = queue.popleft()
+            order.append(v)
+            for i in adj_list[v]:
+                in_degree[i] -= 1
+                if in_degree[i] == 0:
+                    queue.append(i)
+
+        print("Sortowanie Topologiczne (BFS tabela krawedzi):")
+        print(' '.join(map(str, order)))
 
 def create_graph():
-    n = int(input('Podaj liczbę wierzchołków: '))
+    n = int(input('Podaj liczbe wierzcholkow: '))
     graph = Graph(n)
     print('Podaj macierz sasiedztwa: \n')
     for i in range(n):
@@ -146,8 +271,8 @@ def create_graph():
                 graph.add_edge(i, j)
     return graph
 
-
-def generate_graph(saturation, n):  # generowanie grafu acyklicznego
+def generate_graph(saturation):  # generowanie grafu acyklicznego
+    n = int(input('Podaj liczbe wierzcholkow: '))
     list_len = int(n * (n - 1) / 2)
     num_ones = int(list_len * saturation)
     num_zeros = list_len - num_ones
@@ -168,50 +293,100 @@ def generate_graph(saturation, n):  # generowanie grafu acyklicznego
     return g
 
 
-# graph = create_graph()
-# graph.show_matrix()
-# graph.print_successors()
-# graph.dfs()
 
-graph = generate_graph(0.5, 10)
 
-graph.show_matrix()
-graph.show_edges()
-graph.show_successors()
-# PRZESZUKIWANIE BFS
-timer = time()
-graph.bfs_matrix()
-timer = time() - timer
-print(f"Czas BFS dla macierzy sasiedztwa {timer}")
-print()
+option = 1
+while option != 0:
+    print('Wybierz opcję z Menu:\n'
+          '1 Wygeneruj spójny skierowany graf acykliczny\n'
+          '2 Utwórz graf na podstawie macierzy sąsiedztwa\n'
+          '3 Wyświetl macierz sąsiedztwa, listę następników, tabelę krawędzi\n'
+          '4 BFS\n'
+          '5 DFS\n'
+          '6 Sortowanie topologiczne\n'
+          '0 Zakończ program')
+    option = int(input())
+    if option == 1:
+        graph = generate_graph(0.5)
+    elif option == 2:
+        graph = create_graph()
+    elif option == 3:
+        graph.show_matrix()
+        graph.show_edges()
+        graph.show_successors()
+    elif option == 4:
+        timer = time()
+        graph.bfs_matrix()
+        timer = time() - timer
+        print(f"Czas BFS dla macierzy sąsiedztwa: {timer}")
+        print()
 
-timer = time()
-graph.bfs_edges()
-timer = time() - timer
-print(f"Czas BFS dla listy krawedzi {timer}")
-print()
+        timer = time()
+        graph.bfs_edges()
+        timer = time() - timer
+        print(f"Czas BFS dla listy krawędzi: {timer}")
+        print()
 
-timer = time()
-graph.bfs_successors()
-timer = time() - timer
-print(f"Czas BFS dla listy nastepnikow {timer}")
-print()
+        timer = time()
+        graph.bfs_successors()
+        timer = time() - timer
+        print(f"Czas BFS dla listy następników: {timer}")
+        print()
+    elif option == 5:
+        timer = time()
+        graph.dfs_matrix()
+        timer = time() - timer
+        print(f"Czas DFS dla macierzy sąsiedztwa: {timer}")
+        print()
 
-# PRZESZUKIWANIE DFS
-timer = time()
-graph.dfs_matrix()
-timer = time() - timer
-print(f"Czas DFS dla macierzy sasiedztwa {timer}")
-print()
+        timer = time()
+        graph.dfs_edges()
+        timer = time() - timer
+        print(f"Czas DFS dla listy krawędzi: {timer}")
+        print()
 
-timer = time()
-graph.dfs_edges()
-timer = time() - timer
-print(f"Czas DFS dla listy krawedzi {timer}")
-print()
+        timer = time()
+        graph.dfs_successors()
+        timer = time() - timer
+        print(f"Czas DFS dla listy następników: {timer}")
+        print()
+    elif option == 6:
+        timer = time()
+        graph.topological_sort_dfs_matrix()
+        timer = time() - timer
+        print(f"Czas sortowania topologicznego DFS dla macierzy sąsiedztwa: {timer}")
+        print()
 
-timer = time()
-graph.dfs_successors()
-timer = time() - timer
-print(f"Czas DFS dla listy nastepnikow {timer}")
-print()
+        timer = time()
+        graph.topological_sort_bfs_matrix()
+        timer = time() - timer
+        print(f"Czas sortowania topologicznego BFS dla macierzy sąsiedztwa: {timer}")
+        print()
+
+        timer = time()
+        graph.topological_sort_dfs_edges()
+        timer = time() - timer
+        print(f"Czas sortowania topologicznego DFS dla listy krawędzi: {timer}")
+        print()
+
+        timer = time()
+        graph.topological_sort_bfs_edges()
+        timer = time() - timer
+        print(f"Czas sortowania topologicznego BFS dla listy krawędzi: {timer}")
+        print()
+
+        timer = time()
+        graph.topological_sort_dfs_successors()
+        timer = time() - timer
+        print(f"Czas sortowania topologicznego DFS dla listy następników: {timer}")
+        print()
+
+        timer = time()
+        graph.topological_sort_bfs_successors()
+        timer = time() - timer
+        print(f"Czas sortowania topologicznego BFS dla listy następników: {timer}")
+        print()
+    elif option == 0:
+        print('Zakończono program')
+    else:
+        print('Nie rozpoznano opcji')
